1f2fe6b8eeb9c596e7380fb830dfd6009d75beaa
asmit nepali <mail2asmitnepali@gmail.com>
2021-03-17 14:19:25 +0545

    [IMP] PHP: PHP & HTML

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
-<?php 
-	$greeting = "Hello Universe"; 	
-	$name = "Asmit";
-	echo "{$greeting}"." I'm here";
-	echo "{$hello}, {$greeting}";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP-Tutorial</title>
+</head>
+<style type="text/css">
+	h1{
+		background: gray;
+		padding: 2em;
+		text-align: center;
+	}
+</style>
+<body>
+	<header>
+		<h1>
+			<?php
+				$name = "Asmit";
+				echo "My name is ".$name;
+			?>
+		</h1>
+	</header>
+</body>
+</html>
