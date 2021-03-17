1f8f558e04c05cd66b3e57dae6dfbb2582bcde94
asmit nepali <mail2asmitnepali@gmail.com>
2021-03-17 22:06:46 +0545

    [IMP] PHP: class.

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-	$task = [
-			'title' => 'Paypal Payment getway',
-			'due' => 'Today',
-			'status' => true
-		];
-require('index.view.php');
+require 'function.php';
+require 'Task.php';
+$pdo = connectToDB();
+$statement = $pdo->prepare('select * from todo');
+$statement->execute();
+$tasks = $statement->fetchAll(PDO::FETCH_CLASS,'Task'); // using FETCH_CLASS we can access all the punblic property of Task class
+// var_dump($statement->fetchAll(PDO::FETCH_OBJ));
+require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -4,11 +4,11 @@
 	<title>PHP-Tutorial</title>
 </head>
 <style type="text/css">
-	h1{
+	/*h1{
 		background: gray;
 		padding: 2em;
 		text-align: center;
-	}
+	}*/
 </style>
 <body>
 	<header>
@@ -17,15 +17,10 @@
 			</ul>
 		</h1>
 	</header>
-	<?php
-		if($task['status'])
-		{
-			echo "&#9989";
-		} 
-		else
-		{
-			echo "Task Incomplete.";
-		}
-	?>
+	<ul>
+		<?php foreach($tasks as $task):?>
+			<li><?= $task->description; ?></li>
+		<?php endforeach?>
+	</ul>
 </body>
 </html>
