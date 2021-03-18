960fd79314d1971d46593f4a805860b3826ce4fe
asmit nepali <mail2asmitnepali@gmail.com>
2021-03-18 10:15:01 +0545

    [IMP] PHP: QueryBuilder Improvement

--- database/QueryBuilder.php
+++ database/QueryBuilder.php
@@ -5,11 +5,11 @@
 		{
 			$this->pdo = $pdo;
 		}
-		public function selectAll($table)
+		public function selectAll($table,$intoClass)
 		{
 			$statement = $this->pdo->prepare("select * from {$table}");
 			$statement->execute();
-			return $statement->fetchAll(PDO::FETCH_CLASS); 
+			return $statement->fetchAll(PDO::FETCH_CLASS, $intoClass); 
 			// using FETCH_CLASS we can access all the punblic property of Task class
 		}
 	}
--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 
 $query = require 'bootstrap.php';
 require 'Task.php';
-$tasks = $query->selectAll('todo');
+$tasks = $query->selectAll('todo','Task'); //Select from todo and map into Task Class
 require 'index.view.php';
