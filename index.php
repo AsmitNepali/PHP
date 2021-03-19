ff3cb046f762675585d74afb2cc3d7508f9ef8b8
asmit nepali <mail2asmitnepali@gmail.com>
2021-03-19 20:38:23 +0545

    [IMP] PHP: Create direct() method.

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ $query = require 'core/bootstrap.php';
 $router = new Router();
 
 require 'routes.php';
-require $router->direct('contact');
+$uri = trim($_SERVER['REQUEST_URI'],'/');
+require $router->direct($uri);
 
